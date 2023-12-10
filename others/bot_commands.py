85797264fa3e56b7f7738b6c7a45809595143432
aevmed <152068040+aevmed@users.noreply.github.com>
2023-12-10 15:50:52 +0300

    Update bot_commands.py

--- others/bot_commands.py
+++ others/bot_commands.py
@@ -3,10 +3,12 @@ from aiogram import Bot
 from aiogram.client.bot import BotCommand, BotCommandScopeDefault
 
 
+# Списод команд юзера
 user_commands = [
     BotCommand(command='start', description='📕 Перезапустить бота.'),
 ]
 
 
+# Создаем команды
 async def set_commands(bot: Bot):
     await bot.set_my_commands(commands=user_commands, scope=BotCommandScopeDefault())
