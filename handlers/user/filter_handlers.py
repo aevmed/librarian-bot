7dfbe76a9af39267ce953d3747d5b3414de545ad
aevmed <152068040+aevmed@users.noreply.github.com>
2023-12-10 17:07:58 +0300

    Update filter_handlers.py

--- handlers/user/filter_handlers.py
+++ handlers/user/filter_handlers.py
@@ -4,16 +4,18 @@ from filters import IsNotAnEmptyBookList, IsNotAnEmptyMyBookList
 from aiogram import types, F
 
 
+# Обработчик фильтра списка книг
 @user_router.message(IsNotAnEmptyMyBookList(), F.text == '🗑️ Удалить книгу')
 async def delete_books_handler(message: types.Message):
     await message.answer('<b>⭕ К сожалению, список Ваших книг пуст :(</b>')
 
 
+# Обработчик фильтра списка моих книг
 @user_router.message(IsNotAnEmptyBookList(), F.text == '📃 Список всех книг')
 async def filter_book_list_message_handler(message: types.Message):
     await message.answer('<b>⭕ К сожалению, список книг пуст :(</b>')
 
-
+# Обработчик фильтра списка для поиска книг
 @user_router.message(IsNotAnEmptyBookList(), F.text == '🔎 Поиск книги')
 async def filter_find_book_message_handler(message: types.Message):
     await message.answer('<b>⭕ К сожалению, список книг пуст :(</b>')
